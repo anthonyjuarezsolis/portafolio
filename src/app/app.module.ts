b0fd7fe87858eae30fea7ef9eb27ccdd4ac4b4c5
anthonyjuarezsolis <anthonyjuarezsolis@gmail.com>
2018-08-12 14:38:50 -0500

    0.30
    sec 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// rutas
+import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -20,7 +24,8 @@ import { ItemComponent } from './pages/item/item.component';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
