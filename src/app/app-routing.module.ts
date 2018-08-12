113804fe9aeade4565bdfb2feeeec9135942b3d4
anthonyjuarezsolis <anthonyjuarezsolis@gmail.com>
2018-08-12 16:06:15 -0500

    Fin sección 3
    v0.31

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { ItemComponent } from './pages/item/item.component';
 
 // para agregar externcion en el navegador
 const app_routes: Routes = [
-    { path: '', component: PortafolioComponent },
+    { path: 'home', component: PortafolioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'item', component: ItemComponent },
-    { path: '**', pathMatch: 'full', redirectTo: '' }
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 
